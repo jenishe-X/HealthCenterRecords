8ce96e05bccab3c434e169eee1868f34066c1c66
JenisheSalem <jenishedufsalem@gmail.com>
2024-11-17 04:19:15 +0800

    update personnel dashboard

--- frontend/src/lib/NavBar.svelte
+++ frontend/src/lib/NavBar.svelte
@@ -2,7 +2,7 @@
     import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button, Input } from 'flowbite-svelte';
   </script>
   
-  <Navbar style="background-color: #0F2A1D" class="p-2">
+  <Navbar style="background-color: #0F2A1D" class="p-2 z[20]">
     <NavBrand href="/">
       <!-- <img src="/images/flowbite-svelte-icon-logo.svg" class="me-3 h-6 sm:h-9" alt="Flowbite Logo" /> -->
       <span class="self-center whitespace-nowrap text-xl font-semibold text-white">Flowbite</span>
