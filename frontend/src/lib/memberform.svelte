9e60a8d2bc3f6535519cd4577b8aeac5d98b65b7
JenisheSalem <jenishedufsalem@gmail.com>
2024-11-19 23:10:13 +0800

    Member form update

--- frontend/src/lib/memberform.svelte
+++ frontend/src/lib/memberform.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
     import { onMount, onDestroy } from 'svelte';
-    import { Label, Input, InputAddon, ButtonGroup, Select, Textarea, Helper } from 'flowbite-svelte';
-    import { UserCircleSolid, MapPinAltSolid, LandmarkSolid } from 'flowbite-svelte-icons';
+    import { Label, Input, InputAddon, ButtonGroup, Select, Textarea, Helper, Radio } from 'flowbite-svelte';
+    import { UserCircleSolid, MapPinAltSolid, LandmarkSolid, ProfileCardSolid } from 'flowbite-svelte-icons';
     import { Datepicker, P } from 'flowbite-svelte';
+    import { Button, Dropdown, DropdownItem} from 'flowbite-svelte';
+    import { ChevronDownOutline } from 'flowbite-svelte-icons';
+    let group1 = "Family Role";
     
     let name = '';
     let contactNumber = '';
     let location = '';
     let landmark = '';
-    let otherAssistance = '';
     let selected: string = '';
     let selectedDate: Date | null = null;
+    let selectedValue = '';
+    let philmember = '';
+    let philbday: Date | null = null;
+    let philDM = '';
+
   
     let textareaprops = {
       id: 'description',
@@ -19,12 +26,17 @@
       rows: 2,
       placeholder: 'Optional - You may describe the incident...'
     };
+
+    let philhealth = [
+        {value: 'WithPhilhealth', name: 'With Philhealth'},
+        {value: 'NoPhilhealth', name: 'No Philhealth'}
+    ];
   
-    let assistance = [
-      { value: 'Medical Aid', name: 'Medical Aid' },
-      { value: 'Rescue', name: 'Rescue' },
-      { value: 'Fire Incident', name: 'Fire Incident' },
-      { value: 'Road Accident', name: 'Road Accident' },
+    let familyrole = [
+      { value: 'Father', name: 'Father' },
+      { value: 'Mother', name: 'Mother' },
+      { value: 'Child-1', name: 'Child-1' },
+      { value: 'Child-2', name: 'Child-2' },
       { value: 'Other', name: 'Other' }
     ];
   
@@ -78,13 +90,6 @@
         errors.selected = '';
       }
   
-      if (selected === 'Other' && !otherAssistance) {
-        errors.otherAssistance = 'Please specify the assistance';
-        valid = false;
-      } else {
-        errors.otherAssistance = '';
-      }
-  
       return valid;
     };
   
@@ -101,11 +106,11 @@
   </script>
   
   <main class="relative">
-    <div class="bg-white mx-auto absolute top-10 left-0 right-0 p-5 rounded-lg shadow-md max-w-3xl z-10">
-      <div class="grid grid-cols-3 gap-6">
-        <!-- Name -->
+    <div class="bg-white mt-10 mb-10 mx-auto absolute top-10 left-0 right-0 p-5 rounded-lg shadow-md max-w-2xl z-10">
+        <div class="grid grid-cols-2 gap-6">
+        <!-- Last Name -->
         <div class="mb-1">
-          <Label for="name" class="block mb-2">Name</Label>
+          <Label for="name" class="block mb-2">First Name</Label>
           <ButtonGroup class="w-full">
             <InputAddon>
               <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
@@ -117,13 +122,10 @@
           {/if}
         </div>
   
-        <!-- Contact Number -->
+        <!-- Last Name -->
         <div class="mb-1">
-          <Label for="contact-number" class="block mb-2">First Name</Label>
+          <Label for="contact-number" class="block mb-2">Last Name</Label>
           <ButtonGroup class="w-full">
-            <InputAddon>
-                <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
-              </InputAddon>
             <Input id="contact-number" bind:value={contactNumber} placeholder="Juan" />
           </ButtonGroup>
           {#if errors.contactNumber}
@@ -131,7 +133,7 @@
           {/if}
         </div>
   
-        <!-- Location -->
+        <!-- Middle Name -->
         <div class="mb-1">
           <Label for="location" class="block mb-2">Middle Name</Label>
           <ButtonGroup class="w-full">
@@ -148,10 +150,145 @@
             <Datepicker bind:value={selectedDate}/>
         </div>
 
+         <!--Mother's Name-->
+         <div class="mb-1">
+            <Label for="contact-number" class="block mb-2">Mother's Name</Label>
+            <ButtonGroup class="w-full">
+              <InputAddon>
+                  <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                </InputAddon>
+              <Input id="contact-number" bind:value={contactNumber} placeholder="Full Name" />
+            </ButtonGroup>
+            {#if errors.contactNumber}
+              <Helper class="mt-2" color="red">{errors.contactNumber}</Helper>
+            {/if}
+          </div>
+
+          <!--Father's Name-->
+          <div class="mb-1">
+            <Label for="contact-number" class="block mb-2">Father's Name</Label>
+            <ButtonGroup class="w-full">
+              <InputAddon>
+                  <UserCircleSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                </InputAddon>
+              <Input id="contact-number" bind:value={contactNumber} placeholder="Full Name" />
+            </ButtonGroup>
+            {#if errors.contactNumber}
+              <Helper class="mt-2" color="red">{errors.contactNumber}</Helper>
+            {/if}
+          </div>
 
 
+        <!--Contact Number -->
+        <div class="mb-1">
+            <Label for="contact-number" class="block mb-2">Contact Number</Label>
+            <ButtonGroup class="w-full">
+              <InputAddon>(+63)</InputAddon>
+              <Input id="contact-number" bind:value={contactNumber} placeholder="Enter your number" />
+            </ButtonGroup>
+            {#if errors.contactNumber}
+              <Helper class="mt-2" color="red">{errors.contactNumber}</Helper>
+            {/if}
+          </div>
 
+        <!--Sex-->
+        <div class="mt-4 flex items-center space-x-4">
+            <Label for="sex" class="block">Sex: </Label>
+            <div class="flex space-x-6">
+              <Radio name="example1" value="1" bind:group={selectedValue}>Male</Radio>
+              <Radio name="example1" value="2" bind:group={selectedValue}>Female</Radio>
+            </div>
+          </div>  
+        
+        <!--Purok-->
+        <Label>
+            Purok
+            <Select class="mt-2" bind:value={selected}>
+              {#each philhealth as phil}
+                <option value={phil.value}>{phil.name}</option>
+              {/each}
+            </Select>
+          </Label>
+          {#if errors.selected}
+            <Helper class="mt-2" color="red">{errors.selected}</Helper>
+          {/if}
+
+         <!-- Address -->
+         <div class="mb-1">
+            <Label for="contact-number" class="block mb-2">Address</Label>
+            <ButtonGroup class="w-full">
+                <InputAddon>
+                    <MapPinAltSolid class="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                </InputAddon>
+              <Input id="contact-number" bind:value={contactNumber} placeholder="Address" />
+            </ButtonGroup>
+            {#if errors.contactNumber}
+              <Helper class="mt-2" color="red">{errors.contactNumber}</Helper>
+            {/if}
+          </div>
+        
+        <!--Family Role-->
+        <Label>
+            Family Role
+            <Select class="mt-2" bind:value={selected}>
+              {#each familyrole as role}
+                <option value={role.value}>{role.name}</option>
+              {/each}
+            </Select>
+          </Label>
+          {#if errors.selected}
+            <Helper class="mt-2" color="red">{errors.selected}</Helper>
+          {/if}
+      
+        <!--Philhealth-->
+        <Label>
+            Philhealth
+           
+            <Select class="mt-2" bind:value={selected}>
+              {#each philhealth as phil}
+                <option value={phil.value}>{phil.name}</option>
+              {/each}
+            </Select>
+          </Label>
+          {#if errors.selected}
+            <Helper class="mt-2" color="red">{errors.selected}</Helper>
+          {/if}
+
+        <!--With Philhealth-->
+        {#if selected === 'WithPhilhealth'}
+            <div class="-mt-1">
+              <Label for="other-assistance" class="block mb-2">Philhealth Member:</Label>
+              <Input id="other-assistance" bind:value={philmember} placeholder="Philhealth Member" />
+            </div>
+            <div class="-mt-1">
+                <Label for="landmark" class="block mb-2">Member's Birthday</Label>
+                <Datepicker bind:value={selectedDate}/>
+            </div>
+            <div class="-mt-1">
+                <div class="mt-4 flex items-center space-x-4">
+                    <Label for="sex" class="block">Status: </Label>
+                    <div class="flex space-x-6">
+                      <Radio name="example1" value="1" bind:group={selectedValue}>Member</Radio>
+                      <Radio name="example1" value="2" bind:group={selectedValue}>Dependent</Radio>
+                    </div>
+                  </div>   
+            </div>
+          {/if}
       </div>
+
+    <!--Buttons-->
+    <div class="mt-4 flex justify-center space-x-4">
+        <button on:click={handleSubmit} class="text-white rounded-full px-6 py-3 bg-green-950">
+            Cancel &rarr;
+        </button>
+        <button on:click={handleSubmit} class="text-white rounded-full px-6 py-3 bg-green-950">
+          Submit &rarr;
+        </button>
+       
+      </div>
+    
+
     </div>
+
   </main>
   
--- frontend/src/routes/MemberForm/+page.svelte
+++ frontend/src/routes/MemberForm/+page.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import Memberform from "$lib/memberform.svelte";
     import NavBar from "$lib/NavBar.svelte";
+    import Footer from "$lib/footer.svelte";
 </script>
 
 <NavBar/>
 <main>
 <Memberform/>
+
 </main>
+
